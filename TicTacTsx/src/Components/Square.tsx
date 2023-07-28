d3260ccd847ea49f6ea32d9eac0605dbbe44f661
MauricioZavalaAr <mauricio.zavala.ar@gmail.com>
2023-07-27 22:03:57 -0600

    Changes day 27

--- TicTacTsx/src/Components/Square.tsx
+++ TicTacTsx/src/Components/Square.tsx
@@ -1,13 +1,19 @@
 import React from 'react'
 
 interface SquareProps {
-  value: string;
+  value: string | null;
+  onSquareClick: () => void;
 }
 
-
-const Square: React.FC<SquareProps> = ({ value }) => (
-    <button className="square">{value}</button>
+const Square: React.FC<SquareProps> = ({ value, onSquareClick }) => {
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
   );
-  
+};
 
-export default Square
+export default Square;
+
+
+
